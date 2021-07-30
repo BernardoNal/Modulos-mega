2d559b1a7363511d321da49be654ece4303978f2
BernardoNal <50677768+BernardoNal@users.noreply.github.com>
2021-07-29 23:06:17 -0300

    v1.2

--- Importar_excel/php/processoAtualizar.php
+++ Importar_excel/php/processoAtualizar.php
@@ -2,81 +2,62 @@
 	include_once("conexao.php");
 	
 	$Tabela=$_POST['Tabela'];
-	$tables = 'SHOW TABLES';
-	$result = mysqli_query($conn,$tables);
-	$erro_tabela=1;
-	while($row = mysqli_fetch_array($result)){
-	  $name = $row['Tables_in_ffb'];
-	  //Confere se nome tabela existe
-	  if($name == $Tabela){ 
-		 // echo 'Tabela '.$name.'<br>'; 
-		  $erro_tabela=0;
-
 		if(!empty($_FILES['arquivo']['tmp_name'])){
 			$arquivo = new DomDocument();
 			$arquivo->load($_FILES['arquivo']['tmp_name']);
-			//var_dump($arquivo);
 			$linhas = $arquivo->getElementsByTagName('Row');
-			//var_dump($linhas);
 			$celulas = $arquivo->getElementsByTagName('Cell');
-			//var_dump($celulas);
 		    $colunas =(count($celulas)/count($linhas));
 			
 			$query="TRUNCATE TABLE $Tabela"; //Limpeza de Tabela
 			$result = mysqli_query($conn,$query);
-			if($result){
-				//echo 'Tabela Limpa <br>';				
+			if($result){			
 				$searchString = ' ';
 				$replaceString = '';	
 				$primeira_linha = true;
 				$n_inserts=0;
 				foreach($linhas as $linha){
-					//Inserção das linhas da Tabela
+					
 					$tag_linha=$linha->getElementsByTagName('Data');
 					if($primeira_linha == false){
+						//Recebendo linhas a serem inseridas
 						for($i = 0; $i < $colunas; $i++){
 							if(is_object($tag_linha->item($i))){
-							$aux_var = $tag_linha->item($i)->nodeValue;
-							$var[$i] = "'".str_replace("'", $replaceString, $aux_var)."'"; }
+								$aux_var = $tag_linha->item($i)->nodeValue;
+								$var[$i] = "'".str_replace("'", $replaceString, $aux_var)."'"; }
 							else{
 								$var[$i]="' '";
 							}
 						}
 						$valores_coluna=implode(',',$var);
 						$total_valores[$n_inserts]=$valores_coluna;
-						++$n_inserts;
-						
+						++$n_inserts;	
 				}else{
-					//linha de nome das colunas da Tabela
+					//recebe nome da colunas da tabela
 					for($i = 0; $i < $colunas; $i++){
 						$var_tipo[$i] = $tag_linha->item($i)->nodeValue;
 					}
+					//tratamento do nome das colunas
 					$var_coluna=str_replace($searchString, $replaceString,str_replace(".", $replaceString, implode(',',$var_tipo)));
-					//echo $var_coluna;
+					
 				}
 					$primeira_linha = false;
-			}	
+			}
+			//comando de insere todos linhas da tabela
+				$comando_sql= "INSERT INTO  $Tabela($var_coluna)VALUES(".implode('),(',$total_valores).")";
+				$insert = mysqli_query($conn,$comando_sql);
+				if ($insert) {
+					echo "Atualização da tabela concluída";
+				} else {
+					echo "Error: " . mysqli_error($conn)."<br>";
+					  	}	
 			}else{
-				echo "Não foi possível Limpar <br>";
+				echo "Error: " . mysqli_error($conn)."<br>";
+				
 					}
-					$comando_sql= "INSERT INTO  $Tabela($var_coluna)VALUES(".implode('),(',$total_valores).")";
-						/*$insert = mysqli_query($conn,$comando_sql);
-						if ($insert) {
-							echo "atualização concluida";
-							//echo " New record created successfully<hr>";
-					  	} else {
-							echo "Error: <br>" . mysqli_error($conn);
-					  	}*/	
-			//echo "atualização concluida";			
+						
 		}else{
 			echo "erro na leitura da planilha";
-		}break;
-	}
-}
-	if($erro_tabela==1){
-		echo "Essa tabela '$Tabela' não existe";
-	}
+		}
 	mysqli_close($conn);
-	
 ?>
-
--- Importar_excel/php/processoCriar.php
+++ Importar_excel/php/processoCriar.php
@@ -2,57 +2,44 @@
 	include_once("conexao.php");
 	
 	$Tabela=$_POST['Tabela'];
-   
 		if(!empty($_FILES['arquivo']['tmp_name'])){
 			$arquivo = new DomDocument();
 			$arquivo->load($_FILES['arquivo']['tmp_name']);
-			//var_dump($arquivo);
-			$linhas = $arquivo->getElementsByTagName("Row");
-			//var_dump($linhas);
-			$celulas = $arquivo->getElementsByTagName("Cell");
-			//var_dump($celulas);
+			$linhas = $arquivo->getElementsByTagName('Row');
+			$celulas = $arquivo->getElementsByTagName('Cell');
 		    $colunas =(count($celulas)/count($linhas));
 			
-            $searchString = " ";
-            $replaceString = "";
-
+			$searchString = ' ';
+			$replaceString = '';
 			$primeira_linha = true;
-			
+			$n_inserts=0;
 			foreach($linhas as $linha){
-					
-                $tag_linha=$linha->getElementsByTagName('Data');
-					//Inserção das linhas da Tabela
-					if($primeira_linha == false){
-						//echo "<hr>";
-						
-						for($i = 0; $i < $colunas; $i++){
-							if(is_object($tag_linha->item($i))){
-								$aux_var = $tag_linha->item($i)->nodeValue;
-								$var[$i] = "'".str_replace("'", $replaceString, $aux_var)."'"; }
-								else{
-									$var[$i]="' '";
-								}
-							}
-						$valores_coluna=implode(",",$var);
-						//comando de inserção
-						$comando_sql= "INSERT INTO  $Tabela($var_coluna)VALUES($valores_coluna)";
-						$insert = mysqli_query($conn,$comando_sql);
-						if ($insert) {
-							//echo " New record created successfully<hr>";
-					  	} else {
-							echo "Error: <br>" . mysqli_error($conn);
-					  	}
-						//echo $comando_sql."<br>";
+				
+				$tag_linha=$linha->getElementsByTagName('Data');
+				if($primeira_linha == false){
+					//Recebendo linhas a serem inseridas
+					for($i = 0; $i < $colunas; $i++){
+						if(is_object($tag_linha->item($i))){
+							$aux_var = $tag_linha->item($i)->nodeValue;
+							$var[$i] = "'".str_replace("'", $replaceString, $aux_var)."'"; }
+						else{
+							$var[$i]="' '";
+						}
+					}
+					$valores_coluna=implode(',',$var);
+					$total_valores[$n_inserts]=$valores_coluna;
+					++$n_inserts;	
                 }else{
                     //recebe nome da colunas da tabela
 					for($i = 0; $i < $colunas; $i++){
 							$aux = $tag_linha->item($i)->nodeValue;
-                           // $aux2=str_replace("ó", "o", $aux);
-                            $aux3=str_replace(".", $replaceString, $aux);
-                            $var_tipo[$i] = str_replace($searchString, $replaceString, $aux3); 
+                            $aux2=str_replace(".", $replaceString, $aux);
+                            $var_tipo[$i] = str_replace($searchString, $replaceString, $aux2); 
 					}
+					//tratamento do nome das colunas
 					$var_coluna=implode(",",$var_tipo);
 					$var_tipo_coluna=implode(" varchar(255),",$var_tipo);
+					
 					//comando de cria a tabela
 					$comando_sql= "CREATE TABLE  $Tabela( id int NOT NULL primary key AUTO_INCREMENT,$var_tipo_coluna varchar(255))";
 					$insert = mysqli_query($conn,$comando_sql);
@@ -62,28 +49,20 @@
 						echo "Error: <br>" . mysqli_error($conn);
 						break;
 					  }
-					echo $comando_sql."<br>";
+					
                 }
 				$primeira_linha = false;
 			}
-            
-           
-
-           /* $query = "SELECT * FROM $Tabela";
-			$result = mysqli_query($conn,$query);
-		    if($result){
-			while($row = mysqli_fetch_array($result)){
-				$name = $row["nome"];
-				echo "Name: ".$name."<br>";
-			}
-		    }else{
-			echo "n funcionou select 2 <br>";
-		    }*/
+            //comando de insere todos linhas da tabela
+				$comando_sql= "INSERT INTO  $Tabela($var_coluna)VALUES(".implode('),(',$total_valores).")";
+				$insert = mysqli_query($conn,$comando_sql);
+				if ($insert) {
+					echo "Criação da tabela concluída";
+				} else {
+					echo "Error: " . mysqli_error($conn)."<br>";
+					  	}	
 		}else{
 			echo "erro na leitura da planilha";
 		}
-	
-	
 	mysqli_close($conn);
 ?>
-
